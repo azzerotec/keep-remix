87478fe799350eb38d9037771dc6637ff6aef9ca
Mauro Sorgatto <mauro.sorgatto@azzero.tec.br>
2023-05-16 09:13:04 -0300

    add pasta somente para icons

--- app/componets/StudentCategory.tsx
+++ app/componets/StudentCategory.tsx
@@ -1,20 +1,27 @@
 import { Coluna, Linha } from "~/routes/_index";
-import { Lixeiro } from "./Image/Lixo";
-import imgcliente from "./Image/ImageClient.png";
+import { Lixeiro } from "./Image/Icons/icontrash";
+import imgcliente from "./Image/imgcliente.png";
+import { Online } from "./Image/Icons/icononline";
 
 type Props = { title: string; city: string; instrumento: string };
 
 export const StudentCategory = ({ title, city, instrumento }: Props) => {
   return (
-    <Linha>
-      <img src="imgcliente" alt="Icone do Cliente" />
-      <Coluna>
-        <text className="text-base font-bold">{title}</text>
-        <text className="text-xs font-normal text-cinzaclaro">{city}</text>
+    <Linha className="flex items-center">
+      <img src={imgcliente} alt="Icone do Cliente" className="px-2" />
+      <Coluna className="py-2">
+        <text className="mr-9 flex w-full flex-row text-base font-bold">
+          {title}
+        </text>
+        <text className=" text-xs font-normal text-cinzaclaro">{city}</text>
       </Coluna>
-      <text>icone do instrumento</text>
+      <div className="px-4">
+        <Online />
+      </div>
       <text>{instrumento}</text>
-      <Lixeiro />
+      <div className="ml-32">
+        <Lixeiro />
+      </div>
     </Linha>
   );
 };
--- app/routes/createclass.tsx
+++ app/routes/createclass.tsx
@@ -2,9 +2,9 @@ import { Logo } from "~/componets/Image/logo";
 import { CaixaBranca, Coluna, Linha } from "./_index";
 import { StudentCategory } from "~/componets/StudentCategory";
 import imgbateria from "~/componets/Image/imgbaterista.png";
-import { Lupinha } from "~/componets/Image/IcondeBusca";
-import { Pluscircle } from "~/componets/Image/pluscircle";
-import { Setinhas } from "~/componets/Image/setacimaebaixo";
+import { Lupinha } from "~/componets/Image/Icons/IcondeBusca";
+import { Pluscircle } from "~/componets/Image/Icons/iconpluscircle";
+import { Setinhas } from "~/componets/Image/Icons/iconUpDownArrow";
 
 export default function createclass() {
   return (
@@ -18,27 +18,40 @@ export default function createclass() {
         </Linha>
         <Linha>
           <Coluna className="mt-11 pl-28">
-            <h1 className="text-2xl font-bold">Criar Turma</h1>
+            <h1 className="mb-7 text-2xl font-bold">Criar Turma</h1>
             <CaixaBranca className="mr-28 px-8">
               <Linha className="mt-7">
-                <h2 className="text-base font-semibold">
+                <h2 className="w-full text-base font-semibold">
                   Selecionar professor
                 </h2>
-                <Lupinha />
-                <Pluscircle />
+                <Linha className="w-full justify-end ">
+                  <div className="mr-4">
+                    <Lupinha />
+                  </div>
+                  <Pluscircle />
+                </Linha>
               </Linha>
-              <p className="text-xs text-cinzaclaro">32 Professores</p>
-              <Linha>
-                <text>Nome</text>
-                <Setinhas />
-                <text>Categoria</text>
-                <Setinhas />
+              <p className="mt-2 text-xs text-cinzaclaro">32 Professores</p>
+              <Linha className="mt-8 items-center">
+                <Linha className=" w-full  justify-between px-2">
+                  <text>Nome</text>
+                  <Setinhas />
+                </Linha>
+                <Linha className="  px-2">
+                  <h2 className="mr-7">Categoria</h2>
+                  <Setinhas />
+                </Linha>
                 <text>Case branca</text>
               </Linha>
               <StudentCategory
                 title={"Paulo de Oliveira dos Santos"}
                 city={"São Paulo"}
                 instrumento={"Violino"}
+              />
+              <StudentCategory
+                title={"Airton Pereira "}
+                city={"São José do Rio Preto"}
+                instrumento={"Clarinete"}
               />
             </CaixaBranca>
           </Coluna>
