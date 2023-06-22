549c44a04ef2f0ddb2950dfca184b6e67db306dd
Mauro Sorgatto <mauro.sorgatto@azzero.tec.br>
2023-06-22 18:01:04 -0300

    fazendo nova tela de registrar estudantes e otimizando o codigo

--- app/componets/StatusClass.tsx
+++ app/componets/StatusClass.tsx
@@ -1,3 +1,6 @@
+import { CaixaBranca, Coluna, Linha } from "~/routes/_index";
+import CircleCinza from "~/componets/Image/CircleCinza.jpg";
+
 type Props = {
   Statusnumber: string;
   Statusname: string;
@@ -5,9 +8,14 @@ type Props = {
 
 export const Statisticsbox = ({ Statusnumber, Statusname }: Props) => {
   return (
-    <>
-      <p className="text-3xl font-bold">{Statusnumber}</p>
-      <p className="text-sm text-cinzaclaro">{Statusname}</p>
-    </>
+    <CaixaBranca className="h-28 w-56">
+      <Linha>
+        <img src={CircleCinza} alt="Fundo Cinza" className="px-3 py-7" />
+        <Coluna className="py-7">
+          <p className="text-3xl font-bold">{Statusnumber}</p>
+          <p className="text-sm text-cinzaclaro">{Statusname}</p>
+        </Coluna>
+      </Linha>
+    </CaixaBranca>
   );
 };
--- app/routes/registerstudents.tsx
+++ app/routes/registerstudents.tsx
@@ -6,16 +6,16 @@ import { ClassCase } from "~/componets/ClassCase";
 import CircleCinza from "~/componets/Image/CircleCinza.jpg";
 import { Statisticsbox } from "~/componets/StatusClass";
 
-export default function Tracking() {
+export default function RegistrerStudent() {
   return (
     <main className="bg-fundo font-sans">
       <UserNameAndLogo />
       <Coluna className="ml-28 mr-24">
         <h1 className="mb-5 mt-14 h-11 w-56 items-center text-2xl font-black">
-          Acompanhamento
+          Alunos
         </h1>
         <Linha className="justify-between">
-          <Statisticsbox Statusnumber={"32"} Statusname={"Estudantes"} />
+          <Statisticsbox Statusnumber={"62"} Statusname={"Estudantes"} />
           <Statisticsbox Statusnumber={"10"} Statusname={"Professor"} />
           <Statisticsbox Statusnumber={"78%"} Statusname={"Frequência"} />
           <Statisticsbox Statusnumber={"15%"} Statusname={"Desistentes"} />
@@ -23,13 +23,13 @@ export default function Tracking() {
         </Linha>
         <CaixaBranca className="mb-7 mt-14 px-10">
           <Linha className="my-14 justify-between py-2 pr-3">
-            <h2 className="text-lg font-extrabold">Turmas em andamento</h2>
-            <p className="text-cinzaclaro">Alunos</p>
+            <h2 className=" text-cinzaclaro">Turmas em andamento</h2>
+            <p className="text-lg font-extrabold">Alunos</p>
             <p className="text-cinzaclaro">Professores</p>
             <Lupinha />
             <div className="flex w-1/2 justify-end">
               <button className=" h-12 w-44 rounded-md bg-azulClaro text-white">
-                Criar Nova Turma
+                Inserir Aluno
               </button>
             </div>
           </Linha>
